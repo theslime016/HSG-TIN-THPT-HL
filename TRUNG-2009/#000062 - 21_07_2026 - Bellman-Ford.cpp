92be1931938c794285520d8b5a1746118d976b1c
slime016 <139322822+theslime016@users.noreply.github.com>
2026-07-21 20:17:12 +0700

    Update #000062 - 21_07_2026 - Bellman-Ford.cpp

--- TRUNG-2009/#000062 - 21_07_2026 - Bellman-Ford.cpp
+++ TRUNG-2009/#000062 - 21_07_2026 - Bellman-Ford.cpp
@@ -5,21 +5,25 @@ struct ed {
   int u, v;
   long long w;
 };
-vector<ed> edge;
+vector<ed> arc;
 vector<long long> dist;
 const long long inf = 4e18;
 int v, e;
 bool Bellman_Ford(int start) {
   dist[start] = 0;
   for (int i = 0; i < v - 1; i++) {
-    for (ed &A : edge) {
+    bool updated = false;
+    for (ed &A : arc) {
       if (dist[A.u] < inf && dist[A.u] + A.w < dist[A.v]) {
         dist[A.v] = dist[A.u] + A.w;
+        updated = true;
       }
     }
+    if (!updated)
+      break;
   }
 
-  for (ed &B : edge) {
+  for (ed &B : arc) {
     if (dist[B.u] < inf && dist[B.u] + B.w < dist[B.v]) {
       return false;
     }
@@ -33,12 +37,12 @@ int main() {
   cin.tie(0);
 
   cin >> v >> e;
-  edge.resize(e);
+  arc.resize(e);
   dist.assign(v + 1, inf);
   for (int i = 0; i < e; i++) {
     int u, v, w;
     cin >> u >> v >> w;
-    edge[i] = {u, v, w};
+    arc[i] = {u, v, w};
   }
 
   if (Bellman_Ford(1)) {
